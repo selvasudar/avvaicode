9b4375f04df34bd37a1f373ddf37cc1a5afc479c
ramalakshmi <ramalashmicse2020@gmail.com>
2020-05-10 10:24:13 -0600

    theme file covered

--- furniture/wp-content/themes/furniture/page-templates/contactus.php
+++ furniture/wp-content/themes/furniture/page-templates/contactus.php
@@ -3,9 +3,62 @@
 	get_header();
 ?>
 
-	<h1>
-		contact
-	</h1>
+	<div id="breadcrumb" class="section">
+			<!-- container -->
+			<div class="container">
+				<!-- row -->
+				<div class="row">
+					<div class="col-md-12">
+						<h3 class="breadcrumb-header">contact</h3>
+						<!--<ul class="breadcrumb-tree">
+							<li><a href="#">Home</a></li>
+							<li class="active">Blank</li>
+						</ul>-->
+					</div>
+				</div>
+				<!-- /row -->
+			</div>
+			<!-- /container -->
+		</div>
+		<!-- /BREADCRUMB -->
+
+		<!-- SECTION -->
+		<div class="section">
+			<!-- container -->
+			<div class="container">
+				<!-- row -->
+				<div class="row">
+					<h3 class="breadcrumb-header">Contact Details</h3>
+					<div class="col-md-6 order-md-last d-flex">
+						<form action="#"  method="post" class="bg-white p-5 contact-form">
+						  <div class="form-group">
+							<input  name="name" type="text" class="form-control" placeholder="Your Name">
+						  </div>
+						  <div class="form-group">
+							<input name="email" type="email" class="form-control" placeholder="Your Email">
+						  </div>
+						  <div class="form-group">
+							<input name="contact" type="text" class="form-control" placeholder="Your contact">
+						  </div>
+						  <div class="form-group">
+							<textarea name="message" id="message" cols="30" rows="7" class="form-control" placeholder="Message"></textarea>
+						  </div>
+						  <div class="form-group">
+							<input name="submit" type="submit" value="Send Message" class="btn btn-primary py-3 px-5">
+						  </div>
+						</form>
+					  
+					  </div>
+				</div>
+				<!-- /row -->
+			</div>
+			<!-- /container -->
+		</div>
+		<!-- /SECTION -->
+
+		<!-- NEWSLETTER -->
+		
+		<!-- /NEWSLETTER -->
 <?php
 	get_footer();
 ?>
