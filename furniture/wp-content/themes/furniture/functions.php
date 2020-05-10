3c05ab123eb514549b51ab79fc18cc0faeff168e
Selvakumar Duraipandian <selva.duraipandian@orangescape.com>
2020-05-10 18:48:24 +0530

    css and js corrected

--- furniture/wp-content/themes/furniture/functions.php
+++ furniture/wp-content/themes/furniture/functions.php
@@ -1,23 +1,32 @@
 <?php
+
 define('TD_URI', get_template_directory_uri());
 define('TD_PATH', get_template_directory());
 define('ASSETS_URI', get_template_directory_uri() . '/assets/');
 define('FONTS_URI', get_template_directory_uri() . '/assets/fonts/');
 define('STYLES_URI', get_template_directory_uri() . '/assets/css/');
-define('IMAGES_URI', get_template_directory_uri() . '/assets/images/');
+define('IMAGES_URI', get_template_directory_uri() . '/assets/img/');
 define('SCRIPTS_URI', get_template_directory_uri() . '/assets/js/');
 $domain = $_SERVER['HTTP_HOST'];	
 define('DOMAIN',$domain); 
 define('COOKIE_EXPIRY', time() + (86400 * 365));
 
 
-//Adding Sripts and CSS 
+//Adding Scripts and CSS 
 add_action( 'wp_enqueue_scripts', 'include_css_js' );
 
 function include_css_js() {
     
-    wp_enqueue_style( 'cssfile', STYLES_URI.'bootstrap/bootstrap.min.css', array(), 234.0 );
+    wp_enqueue_style( 'cssfile1', STYLES_URI.'bootstrap.min.css', array(), 234.0 );
+    wp_enqueue_style( 'cssfile2', STYLES_URI.'slick.css', array(), 234.0 );
+    wp_enqueue_style( 'cssfile3', STYLES_URI.'slick-theme.css', array(), 234.0 );
+    wp_enqueue_style( 'cssfile4', STYLES_URI.'nouislider.min.css', array(), 234.0 );
+    wp_enqueue_style( 'cssfile5', STYLES_URI.'font-awesome.min.css', array(), 234.0 );
+    wp_enqueue_style( 'cssfile6', STYLES_URI.'style.css', array(), 234.0 );
     wp_enqueue_script( 'jsfile1', SCRIPTS_URI.'jquery.min.js', array(), 234.0 );
-    wp_enqueue_script( 'jsfile2', SCRIPTS_URI.'bootstrap/bootstrap.min.js', array(), 234.0 );
+    wp_enqueue_script( 'jsfile1', SCRIPTS_URI.'bootstrap.min.js', array(), 234.0 );
+    wp_enqueue_script( 'jsfile1', SCRIPTS_URI.'slick.min.js', array(), 234.0 );
+    wp_enqueue_script( 'jsfile1', SCRIPTS_URI.'nouislider.min.js', array(), 234.0 );
+    wp_enqueue_script( 'jsfile1', SCRIPTS_URI.'jquery.zoom.min.js', array(), 234.0 );
+    wp_enqueue_script( 'jsfile2', SCRIPTS_URI.'main.js', array(), 234.0 );
 }
-?>
